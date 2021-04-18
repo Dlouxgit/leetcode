2ffebd4f02eb295c71a6ea0ca85c2fb767a0b4b0
dlouxgit <470490778@qq.com>
2021-04-19 00:29:29 +0800

    init

---
+++ 5.最长回文子串.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=5 lang=javascript
+ *
+ * [5] 最长回文子串
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    var a = 1
+    console.log(123)
+    console.log(a)
+    a = 2
+    console.log(a)
+    console.log(123213)
+    a = 3
+    console.log(a)
+};
+// @lc code=end
+
+longestPalindrome()
